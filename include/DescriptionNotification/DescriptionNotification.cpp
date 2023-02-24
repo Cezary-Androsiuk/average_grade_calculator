2435a9e3dc3b0c4df3f665aed358b03cce9b53d0
Cezary-Androsiuk <czandros@wp.pl>
2023-02-24 13:51:00 +0100

    created DescriptionNotification dir

---
+++ include/DescriptionNotification/DescriptionNotification.cpp
@@ -0,0 +1,1 @@
+#include "DescriptionNotification.hpp"
---
+++ include/DescriptionNotification/DescriptionNotification.hpp
@@ -0,0 +1,8 @@
+#ifndef DESCRIPTION_NOTIFICATION_HPP
+#define DESCRIPTION_NOTIFICATION_HPP
+
+class DescriptionNotification{
+    
+};
+
+#endif
